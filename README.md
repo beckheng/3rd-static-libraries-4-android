a3dfdbbcc6037f08036fcab44d09ad78d0cc8c62
beckheng <beckheng@gmail.com>
2014-05-18 13:53:02 +0800

    libevent_static

--- README.md
+++ README.md
@@ -3,11 +3,12 @@
 
 ## 缘起
 
-在使用cocos2d-x开发过程中，有些功能需要用到开源的库，为方便，把这些库用ndk编译为Android可用，iOS下无这些痛苦(实在)的过程
+在使用cocos2d-x开发过程中，有些功能需要用到开源的库，为方便，把这些库用ndk编译为Android可用，~~iOS下无这些痛苦(实在)的过程~~ 随着libevent库的编译，发觉iOS下也会有类似的痛苦
 
-暂时有下面两个库  
-- libsqlite  
-- libuuid  
+## 暂时有下面三个库  
+- libsqlite
+- libuuid
+- libevent_static
 
 ## libsqlite
 使用官方源代码编译  
@@ -15,6 +16,15 @@
 
 ## libuuid
 使用e2fsprogs源代码编译
+
+## libevent_static
+使用2.0.21版本代码编译
+
+## 目录说明
+1. include  
+	头文件路径
+2. lib  
+	静态库文件路径
 
 ## 其它
 **如何在ndk中使用静态库可参考jni/Android.mk**
--- jni/Android.mk
+++ jni/Android.mk
@@ -10,10 +10,22 @@ LOCAL_MODULE := libsqlite
 LOCAL_SRC_FILES := ../lib/libsqlite.a
 include $(PREBUILT_STATIC_LIBRARY)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libevent_static
+LOCAL_SRC_FILES := ../lib/libevent_static.a
+include $(PREBUILT_STATIC_LIBRARY)
+
 #your module here
 #你的模块定义
+
+# header files here
+# 头文件路径
+LOCAL_C_INCLUDES := /path/of/e2fsprogs/lib \
+                    /path/of/sqlite-amalgamation \
+                    //path/of/libevent/include
 
 #remember indicate the static libraries mention above, please see ndk document for syntax
 #切记要加入以下两句,语法请参考ndk文档
 LOCAL_WHOLE_STATIC_LIBRARIES := libuuid
 LOCAL_WHOLE_STATIC_LIBRARIES += libsqlite
+LOCAL_WHOLE_STATIC_LIBRARIES += libevent_static
