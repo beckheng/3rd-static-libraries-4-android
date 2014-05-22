5f57c3a76d866c875226a291341daa3b394b3c46
beckheng <beckheng@gmail.com>
2014-05-22 09:12:38 +0800

    修正拼写错误

--- libevent/build-android.sh
+++ libevent/build-android.sh
@@ -6,7 +6,7 @@
 	LD=arm-linux-androideabi-ld \
 	CPPFLAGS="-I$ANDROID_NDK_ROOT/platforms/android-19/arch-arm/usr/include/" \
 	CFLAGS="-nostdlib" \
-	LDFLAGS="-Wl,-rpath-link=$ANDROID_NDK_ROOT/platforms/android-19/arch-arm/usr/lib/ -L$ANDROID_NKD_ROOT/plaforms/android-19/arch-arm/usr/lib/ --sysroot=$ANDROID_NDK_ROOT/platforms/android-19/arch-arm/" \
+	LDFLAGS="-Wl,-rpath-link=$ANDROID_NDK_ROOT/platforms/android-19/arch-arm/usr/lib/ -L$ANDROID_NDK_ROOT/platforms/android-19/arch-arm/usr/lib/ --sysroot=$ANDROID_NDK_ROOT/platforms/android-19/arch-arm/" \
 	LIBS="-lc -lgcc -L$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.8/prebuilt/darwin-x86_64/lib/gcc/arm-linux-androideabi/4.8"
 
 ndk-build
